5bae7ce15368286a384f5f868467a1afe61eb85a
Matthew Wilson <Matthew Wilson>
2017-02-10 16:55:09 +0000

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+
+const API_KEY = 'AIzaSyA47hYoBdF76VBJuA2chMUQXrkEiwWR-qg';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
